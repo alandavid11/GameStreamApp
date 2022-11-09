29d4561bb192de6c7e748ade5db0d480c951813e
Alan Gloria <102493628+alandavid11@users.noreply.github.com>
2022-11-09 01:25:35 -0600

    added comments section on gameView

--- GameStream/GameStream/Views/gameView.swift
+++ GameStream/GameStream/Views/gameView.swift
@@ -34,6 +34,11 @@ struct gameView: View {
                     // Informacion video
                     videoinfo(titulo: titulo, studio: studio, calificacion: calificacion, anoPublicacion: anoPublicacion, descripcion: descripcion, tags: tags)
                         .padding(.bottom)
+                    
+                    Gallery(imgURL: imgsUrl)
+                    
+                    Comentarios()
+                    
                 }.frame(maxWidth: .infinity)
             }
             
@@ -46,11 +51,20 @@ struct gameView: View {
 
 struct video:View{
     
-    var url:String
-    var body: some View{
+    let url: String
         
-        VideoPlayer(player: AVPlayer(url: URL(string: url)!)).ignoresSafeArea()
-    }
+        init(url: String) {
+            self.url = url
+        }
+        
+        var body: some View {
+            let player = AVPlayer(url: URL(string: url)!)
+            VideoPlayer(player: player)
+                .ignoresSafeArea()
+                .onDisappear(perform: {
+                    player.pause()
+                })
+        }
 }
 
 struct videoinfo:View{
@@ -108,6 +122,135 @@ struct videoinfo:View{
             
         }.frame(maxWidth: .infinity,alignment: .leading)
         
+    }
+}
+
+struct Gallery:View{
+    
+    var imgURL:[String]
+    let formaGrid = [GridItem(.flexible())]
+    
+    @ViewBuilder
+    func placeholderImage() -> some View {
+        EmptyView()
+    }
+    
+    var body: some View{
+        
+        VStack(alignment: .leading){
+            
+            Text("GALERÍA")
+                .foregroundColor(.white)
+                .font(.title)
+                .padding(.leading)
+            
+            ScrollView(.horizontal){
+                
+                LazyHGrid(rows: formaGrid, spacing: 8){
+                    ForEach(imgURL, id:\.self){
+                        
+                        imgURL in
+                        
+                        // Desplegar imagenes del servidor por medio de URL
+                        AsyncImage(url: URL(string: imgURL)) {
+                            image in
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        } placeholder: {
+                            placeholderImage()
+                        }
+                    }
+                    
+                }
+                
+            }.frame(height: 180)
+            
+        }.frame(maxWidth: .infinity, alignment: .leading)
+        
+    }
+}
+
+struct Comentarios: View {
+    
+    var body: some View{
+        
+        VStack(alignment: .leading){
+            
+            Text("COMENTARIOS")
+                .foregroundColor(.white)
+                .font(.title3)
+                .fontWeight(.bold)
+                .padding(.leading)
+                .padding(.top)
+            
+            VStack {
+                
+                HStack{
+                    
+                    Image("picture")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 64, height: 64, alignment: .leading)
+                        .padding(.leading)
+                        .padding(.top)
+                    
+                    VStack{
+                        
+                        Text("Enrique Ramos")
+                            .foregroundColor(.white)
+                            .font(.subheadline)
+                            .fontWeight(.bold)
+                            .frame(alignment:.leading)
+                            .padding(.top)
+                        
+                        Text("Hace 7 días").foregroundColor(.white).font(.footnote).frame(alignment: .leading).padding(.bottom)
+                        
+                        
+                    }
+                    
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                
+                Text("He visto que como media tiene una gran calificación, y estoy completamente de acuerdo. Es el mejor juego que he jugado sin ninguna duda, combina una buena trama con una buenísima experiencia de juego libre gracias a su inmenso mapa y actividades.").foregroundColor(.white).font(.subheadline).frame(alignment: .leading)
+                    .padding(EdgeInsets(top: 0, leading: 15, bottom: 15, trailing: 15))
+                
+            }.frame(maxWidth: .infinity, alignment: .leading).background(Color(red: 34/255, green: 53/255, blue: 94/255, opacity: 1.0)).clipShape(RoundedRectangle(cornerRadius: 8)).padding(.all, 10)
+            
+            VStack {
+                
+                HStack{
+                    
+                    Image("picture")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 64, height: 64, alignment: .leading)
+                        .padding(.leading)
+                        .padding(.top)
+                    
+                    VStack{
+                        
+                        Text("Damon Barlow")
+                            .foregroundColor(.white)
+                            .font(.subheadline)
+                            .fontWeight(.bold)
+                            .frame(alignment:.leading)
+                            .padding(.top)
+                        
+                        Text("Hace 12 días").foregroundColor(.white).font(.footnote).frame(alignment: .leading).padding(.bottom)
+                        
+                        
+                    }
+                    
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                
+                Text("He visto que como media tiene una gran calificación, y estoy completamente de acuerdo. Es el mejor juego que he jugado sin ninguna duda, combina una buena trama con una buenísima experiencia de juego libre gracias a su inmenso mapa y actividades.").foregroundColor(.white).font(.subheadline).frame(alignment: .leading)
+                    .padding(EdgeInsets(top: 0, leading: 15, bottom: 15, trailing: 15))
+                
+            }.frame(maxWidth: .infinity, alignment: .leading).background(Color(red: 34/255, green: 53/255, blue: 94/255, opacity: 1.0)).clipShape(RoundedRectangle(cornerRadius: 8)).padding(.all, 10)
+            
+          
+            
+        }
     }
 }
 
